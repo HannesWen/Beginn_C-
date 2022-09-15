d10b5dbf2984edb723b08326d4b55ed02c90063b
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-15 09:35:04 +0200

    We ask user for his age and ask how old he will be in X years #5

--- Beginning C Sharp/Beginning C Sharp/Program.cs
+++ Beginning C Sharp/Beginning C Sharp/Program.cs
@@ -14,10 +14,14 @@ namespace Beginning_C_Sharp
             string userName = Console.ReadLine();
             Console.WriteLine("Hello " + userName);
 
-            Console.WriteLine("Wie alt bist du ?");
+            Console.WriteLine("Wie alt bist du aktuell ?");
             int age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Du bist also " + age);
+            Console.WriteLine("Wie alt bist du in X Jahren ?");
+            int nextYear = Convert.ToInt32(Console.ReadLine());
+            int sumYears = age + nextYear;
+            Console.WriteLine("Du bist also in  " + nextYear + " Jahren, " + sumYears + " Jahre alt!");
+
             Console.ReadLine();
         }
     }
